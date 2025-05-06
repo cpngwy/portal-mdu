40de63d7faf18022ef4cc1b160b326b24852373e
EC2 Default User <cpngwy@gmail.com>
2025-05-06 00:26:56 +0000

    Update pie chart value

--- public/themes/sb-admin-2-gh-pages/js/demo/chart-pie-demo.js
+++ public/themes/sb-admin-2-gh-pages/js/demo/chart-pie-demo.js
@@ -7,11 +7,11 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Pending", "Processing", "Approved", "Declined"],
     datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
+      data: [15, 30, 50, 5],
+      backgroundColor: ['#858796', '#4e73df', '#1cc88a', '#e74a3b'],
+      hoverBackgroundColor: ['#c9cecd', '#addae1', '#9ce6cb', '#fea9a1'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
   },
